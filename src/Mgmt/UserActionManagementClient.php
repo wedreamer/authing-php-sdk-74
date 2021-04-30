c57d3fb469bd2048fdb74f767b10b5a810e48381
shubuzuo <lixintao2@authing.cn>
2021-04-30 21:38:55 +0800

    publish

---
+++ src/Mgmt/UserActionManagementClient.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Authing\Mgmt;
+use Authing\Mgmt\ManagementClient;
+
+class UserActionManagementClient {
+    /**
+     * @var mixed[]
+     */
+    private $options;
+
+    /**
+     * @var ManagementClient
+     */
+    private $client;
+
+    public function __construct(ManagementClient $client)
+    {
+        $this->client = $client;
+        $this->options = $client->options;
+    }
+
+    public function list(array $params = [
+        'page' => 1,
+        'limit' => 10
+    ])
+    {
+        $userPoolId = $this->client->options->userPoolId;
+        $page = $params['page'] ?? 1;
+        $limit = $params['limit'] ?? 10;
+        $att = [
+            'page' => $page,
+            'limit' => $limit,
+            'clientip' => $params['clientIp'],
+            'operation_name' => $params['operationName'],
+            'operator_arn' => $params['operatoArn']
+        ];
+        $qstr = http_build_query($att);
+        $data = $this->client->httpGet("/api/v2/analysis/user-action?$qstr");
+        return $data;
+    }
+
+    public function export()
+    {
+        # code...
+    }
+}
+
